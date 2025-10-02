4f18841659c461d790e655ef3dd323ff771c0006
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-10-02 16:20:17 +0530

    Update and rename Example8.sv to section 1/Example8.sv

--- section 1/Example8.sv
+++ section 1/Example8.sv
@@ -1,18 +1,15 @@
-    `include "uvm_macros.svh"
-    import uvm_pkg::*;
+`include "uvm_macros.svh"
+import uvm_pkg::*;
      
-    //////////////////////////////////////////////////
-    class driver extends uvm_driver;
-      `uvm_component_utils(driver)
+//////////////////////////////////////////////////
+class driver extends uvm_driver;
+    `uvm_component_utils(driver)
       
-      function new(string path , uvm_component parent);
+    function new(string path , uvm_component parent);
         super.new(path, parent);
-      endfunction
-     
-     
-      
-      
-      task run();
+    endfunction
+       
+    task run();
         `uvm_info("DRV", "Informational Message", UVM_NONE);
         `uvm_warning("DRV", "Potential Error");
         `uvm_error("DRV", "Real Error");
@@ -20,13 +17,11 @@
         `uvm_fatal("DRV", "Simulation cannot continue DRV1");
         #10;
         `uvm_fatal("DRV1", "Simulation Cannot Continue DRV1");
-      endtask
+    endtask
       
+endclass
      
-      
-    endclass
-     
-    /////////////////////////////////////////////
+/////////////////////////////////////////////
      
      
     module tb;
@@ -41,4 +36,5 @@
       
       
       
+
     endmodule
