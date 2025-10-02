d0ac5e25ae5858b522dbf2c2d96292dd09376891
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-10-02 15:32:01 +0530

    Update and rename Example2.sv to uvm_reporting_example2.sv

--- uvm_reporting_example2.sv
+++ uvm_reporting_example2.sv
@@ -1,14 +1,12 @@
-    `include "uvm_macros.svh"
-    import uvm_pkg::*;
+`include "uvm_macros.svh"
+import uvm_pkg::*;
      
-    module tb;
+module tb;
       
-      initial begin
+    initial begin
         uvm_top.set_report_verbosity_level(UVM_HIGH);
         $display("Default Verbosity level : %0d ", uvm_top.get_report_verbosity_level);
         `uvm_info("TB_TOP", "String", UVM_HIGH);
-      
-      end
-      
-      
-    endmodule
+    end
+
+endmodule
